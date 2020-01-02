ad77361460f2fa9f6bafcb89ff9d0f409d51936b
Himadri Bhargava <himadribhargava@gmail.com>
2020-01-02 20:53:12 +0530

    refactor dishDetail

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,45 +2,56 @@ import React, { Component, Fragment } from 'react'
 import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
 import { getMonthName } from '../shared/dishes';
 export default class DishDetail extends Component {
+    // eslint-disable-next-line
     constructor(props){
         super(props);
 
     }
-    render() {
-        const { image, name, description, comments } = this.props.dish;
-        const dishComments = comments.map(c=> {
-            if(c != null){
+    renderDish(dish){
+        const { image, name, description } = dish;
+        return(
+            <Card>
+                <CardTitle >{name}</CardTitle>
+                <CardImg width="100%" src={image} alt={name} />
+                <CardBody>
+                    <CardTitle>{name}</CardTitle>
+                    <CardText>{description}</CardText>
+                </CardBody>
+             </Card>
+        )
+    }
+    renderComments(comments) {
+        let dishComments;
+        if(comments != null){
+            dishComments = comments.map(c=> {
                 const day = c.date.substring(8,10);
-                const month = getMonthName(c.date.substring(5,7));
+                //const month = getMonthName(c.date.substring(5,7));
+                const month = c.date.substring(5,7);
                 const year = c.date.substring(0,4);
                 return (
-                    <div key={c.id}>
+                    <li key={c.id} className="list-unstyled">
                         <p>{c.comment}</p>
-                        <p>-- {c.author}, <span>{month} {day}, {year}</span></p>
-                    </div>
+                        <p>-- {c.author}, <span>{month}-{day}-{year}</span></p>
+                    </li>
                 )
-            }
-            else{
-                return(
-                    <div></div>
-                )
-            }
-        });
+            });
+            return dishComments;
+        }
+        else{
+            return (
+                <div></div>
+            )
+        }
+    }
+    render() {
         return (
             <Fragment>
-                <div className="col-md-5 mt-2">
-                    <Card>
-                        <CardTitle >{name}</CardTitle>
-                        <CardImg width="100%" src={image} alt={name} />
-                        <CardBody>
-                            <CardTitle>{name}</CardTitle>
-                            <CardText>{description}</CardText>
-                        </CardBody>
-                    </Card>
+                <div className="col-md-5 m-1">
+                    {this.renderDish(this.props.dish)}
                 </div>
-                <div className="col-md-5 mt-2">
+                <div className="col-md-5 m-1">
                     <h4>Comments</h4>
-                    {dishComments}
+                    {this.renderComments(this.props.dish.comments)}
                 </div>
             </Fragment>
         )
